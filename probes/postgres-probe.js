d1e07e37fe009ee401663fbb7d5eb3f8248cdf1b
Daniel Cunnington <dancunnington@uk.ibm.com>
2015-11-25 13:10:37 +0000

    probe now caters for client pooling. Test cases added

--- probes/postgres-probe.js
+++ probes/postgres-probe.js
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+*******************************************************************************/
 var Probe = require('../lib/probe.js');
 var aspect = require('../lib/aspect.js');
 var request = require('../lib/request.js');
@@ -20,101 +20,106 @@ var util = require('util');
 var am = require('appmetrics');
 
 function PostgresProbe() {
-	Probe.call(this, 'pg');
+  Probe.call(this, 'pg');
 }
+
 util.inherits(PostgresProbe, Probe);
 
+//This method attaches our probe to the instance of the postgres module (target)
+PostgresProbe.prototype.attach = function(name, target) {
 
-/*
-* Attach method
-* ---------------
-* @param name - The name of the npm module
-* @param target - The instance of the target module
-*
-* This method attaches our probe to the instance of the postgres module (target)
-* A sample postgres query has the following format:
-* 
-* var pg = require('pg');
-* var conString = "postgres://user:password@host/database";
-*
-* var client = new pg.Client(conString);
-*
-* client.connect(function(err) {
-*   if(err) {
-*       return err;
-*   }
-*   client.query("SELECT *",function(err,result) {
-*    
-*       //result is the result of the sql query.
-*   }
-* }
-*
-* So, we need to access the client variable in order to monitor queries to the client.
-* After a connection has been established, we start monitoring for any SQL queries
-* using the 'query' method.
-*
-*/
-PostgresProbe.prototype.attach = function( name, target ) {
+  var that = this;
+  if( name != "pg" ) return target;
+  if(target.__ddProbeAttached__) return target;
+  target.__ddProbeAttached__ = true;
 
-	var that = this;
-    if( name != "pg" ) return target;
-    if(target.__ddProbeAttached__) return target;
-    target.__ddProbeAttached__ = true;
+  var data = {};
 
-    //After the client has been instantiated
-    var data = {};
-    aspect.after(target, 'Client', data, function(clientTarget, methodName, methodArgs, probeData, rc) {
+  //There are two methods to connect to a pg instance
+  //Client Pooling or Client Instance. See https://www.npmjs.com/package/pg
 
-        //After a connection has been established on the client
-        aspect.after(clientTarget, 'connect', data, function(connectionTarget, methodName, args, probeData, rc) {
-                       
-            //Before the query hits, start monitoring
-            aspect.before(connectionTarget, 'query',
-                function(target, methodName, methodArgs, probeData) {
-                    var method = 'query';
-                    that.metricsProbeStart(probeData, target, method, methodArgs);
-                    that.requestProbeStart(probeData, target, method, methodArgs);
-                    if (aspect.findCallbackArg(methodArgs) != undefined) {
-                        aspect.aroundCallback(methodArgs, probeData, function(target,args,probeData){
+  //Client Pooling
+  //Before we connect
+  aspect.before(target, 'connect', function(target, methodName, methodArgs, probeData) {
+    
+    //Get client result from connect callback
+    if (aspect.findCallbackArg(methodArgs) != undefined) {
+      aspect.aroundCallback(methodArgs, probeData, function(target, args, probeData) {
 
-                            //Here, the query has executed and returned it's callback. Then
-                            //stop monitoring
-                            that.metricsProbeEnd(probeData, method, methodArgs);
-                            that.requestProbeEnd(probeData, method, methodArgs);
-                        });
-                    };
-                }
-            );
-            return rc;
-        });
+        //Extract client
+        var client = args[1];
+
+        //Connection pooling means we may get clients we have seen before
+        //so only monitor queries on this client if we haven't seen it before
+        if(!client.__appmetricsProbeAttached__) {
+          client.__appmetricsProbeAttached__ = true;
+
+          //Start monitoring
+          monitorQuery(client,that);
+        }
+      });
+    }
+  });
+
+  //Client Instance
+  //After the client has been instantiated
+  aspect.after(target, 'Client', data, function(clientTarget, methodName, methodArgs, probeData, rc) {
+
+    //After a connection has been established on the client
+    aspect.after(clientTarget, 'connect', data, function(connectionTarget, methodName, args, probeData, rc) {
+
+      //Before the query hits, start monitoring
+      monitorQuery(connectionTarget,that);
+      return rc;
     });
-    return target;
-};
+  });
+  return target;
+}
 
+//This function monitors the query method given a connected 
+//client and the current 'PostgresProbe' reference
+function monitorQuery(client,that) {
+  aspect.before(client, 'query',
+    function(target, methodName, methodArgs, probeData) {
+      var method = 'query';
+      that.metricsProbeStart(probeData, target, method, methodArgs);
+      that.requestProbeStart(probeData, target, method, methodArgs);
+      if (aspect.findCallbackArg(methodArgs) != undefined) {
+        aspect.aroundCallback(methodArgs, probeData, function(target,args,probeData){
+
+          //Here, the query has executed and returned it's callback. Then
+          //stop monitoring
+          that.metricsProbeEnd(probeData, method, methodArgs);
+          that.requestProbeEnd(probeData, method, methodArgs);
+        });
+      }
+    }
+  );
+}
 
 /*
  * Lightweight metrics probe for Postgres queries
  * 
  * These provide:
- * 		time:		time event started
- * 		query:		The SQL executed
- * 		duration:	the time for the request to respond
+ *      time:       time event started
+ *      query:      The SQL executed
+ *      duration:   the time for the request to respond
  */
 PostgresProbe.prototype.metricsEnd = function(probeData, method, methodArgs) {
-	probeData.timer.stop();
-    am.emit('postgres', {time: probeData.timer.startTimeMillis, query: methodArgs[0], duration: probeData.timer.timeDelta});
+  probeData.timer.stop();
+  am.emit('postgres', {time: probeData.timer.startTimeMillis, query: methodArgs[0], duration: probeData.timer.timeDelta});
 };
 
 /*
  * Heavyweight request probes for Postgres queries
  */
 PostgresProbe.prototype.requestStart = function (probeData, target, method, methodArgs) {
-    probeData.req = request.startRequest( 'DB', "query", false, probeData.timer );
-    probeData.req.setContext({sql: methodArgs[0]});
+  probeData.req = request.startRequest( 'DB', "query", false, probeData.timer );
+  probeData.req.setContext({sql: methodArgs[0]});
 };
 
 PostgresProbe.prototype.requestEnd = function (probeData, method, methodArgs) {
-	probeData.req.stop({sql: methodArgs[0]});
+  probeData.req.stop({sql: methodArgs[0]});
 };
 
 module.exports = PostgresProbe;
