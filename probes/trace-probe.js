b16120408aad126ce8132f78dccde69ee2b50611
seabaylea <baileyc@uk.ibm.com>
2015-11-26 17:58:58 +0000

    pass 'this'parameter to apply call

--- probes/trace-probe.js
+++ probes/trace-probe.js
@@ -45,7 +45,7 @@ TraceProbe.prototype.attach = function( moduleName, target ) {
         ret = target;
 		if(Object.keys(target.prototype).length==0 && Object.keys(target).length == 0){
 			ret = function () {
-				var rc = target.apply(null, arguments);
+				var rc = target.apply(this, arguments);
 				instrumentMethods(moduleName, rc);
 				return rc;
 			}
