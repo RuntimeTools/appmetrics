29b90395efbf5be5652a2500fa8f9f46f2fde56d
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-02-05 10:26:31 +0000

    Added heap events to appmetrics-api.js

--- appmetrics-api.js
+++ appmetrics-api.js
@@ -14,46 +14,48 @@
  * limitations under the License.
  *******************************************************************************/
 
-var util = require('util');
-var EventEmitter = require('events').EventEmitter;
-var serializer = require('./lib/serializer');
+ var util = require('util');
+ var EventEmitter = require('events').EventEmitter;
+ var serializer = require('./lib/serializer');
 
-function API(agent, appmetrics) {
-	this.appmetrics = appmetrics;
-	this.agent = agent;
+ function API(agent, appmetrics) {
+    this.appmetrics = appmetrics;
+    this.agent = agent;
     this.environment = {};
     /*
      * We consider ourselves initialized when we have both OS and Runtime Env data
      * Decrement on when we get each, and raise 'initialized' event on 0;
      */
-    this.initialized = 2;
-    var that = this;
+     this.initialized = 2;
+     var that = this;
 
-    var raiseEvent = function (topic, message) {
+     var raiseEvent = function (topic, message) {
         if (topic === 'common_cpu' || topic === 'cpu') {
-                formatCPU(message);
+            formatCPU(message);
         } else if (topic === 'common_env') {
-                formatOSEnv(message);
+            formatOSEnv(message);
         } else if (topic === 'environment_node') {
-                formatRuntimeEnv(message);
+            formatRuntimeEnv(message);
         } else if (topic === 'common_memory' || topic === 'memory') {
-                formatMemory(message);
+            formatMemory(message);
         } else if (topic === 'gc_node') {
-                formatGC(message);
+            formatGC(message);
+        } else if (topic === 'heap_node') {
+            formatHeap(message);        
         } else if (topic === 'profiling_node') {
-        		formatProfiling(message);
+            formatProfiling(message);
         } else if (topic === 'api') {
-                formatApi(message);
+            formatApi(message);
         } else {
         /*
          * Just raise any unknown message as an event so someone can parse it themselves
          */
-                that.emit(topic, message);
-        }
-    };
+         that.emit(topic, message);
+     }
+ };
 
-    var formatCPU = function(message) {
-    	// cpu : startCPU@#1412609879696@#0.000499877@#0.137468
+ var formatCPU = function(message) {
+        // cpu : startCPU@#1412609879696@#0.000499877@#0.137468
         var values = message.trim().split('@#'); // needs to be trimmed because of leading \n character
         var cpu = {time: parseInt(values[1]), process: parseFloat(values[2]), system: parseFloat(values[3])};
         that.emit('cpu', cpu);
@@ -71,30 +73,30 @@ function API(agent, appmetrics) {
            native.library.date=Oct 20 2014 10:51:56
            number.of.processors=2
            command.line=/home/exampleuser/sandbox/node-v0.10.32-linux-x64/bin/node /home/exampleuser/sandbox/node-v0.10.32-linux-x64/lib/node_modules/appmetrics/launcher.js red.js
-         */
-		var values = message.split('\n');
-    	var env = {};
-        values.forEach(function (value) {
-        	if (value[0] != '#'){
-        		var terms = value.split('=');
-        		env[terms[0]] = terms[1];
-        	}
+           */
+           var values = message.split('\n');
+           var env = {};
+           values.forEach(function (value) {
+            if (value[0] != '#'){
+                var terms = value.split('=');
+                env[terms[0]] = terms[1];
+            }
         });
 
-        setEnv(env);
-        that.initialized--;
-        that.emit('environment', that.environment);
-        if (that.initialized == 0) that.emit('initialized');
-    };
+           setEnv(env);
+           that.initialized--;
+           that.emit('environment', that.environment);
+           if (that.initialized == 0) that.emit('initialized');
+       };
 
-    var setEnv = function(env) {
-    	for (var p in env) {
+       var setEnv = function(env) {
+        for (var p in env) {
             that.environment[p] = env[p];
         }
     };
 
     var formatRuntimeEnv = function(message) {
-    	//environment_node : #EnvironmentSource
+        //environment_node : #EnvironmentSource
         //runtime.version=v0.10.32-IBMBuild-201410132030
         //runtime.vendor=IBM
         //runtime.name=IBM SDK for Node.js
@@ -103,9 +105,9 @@ function API(agent, appmetrics) {
         var values = message.trim().split('\n');
         var result = {};
         values.forEach(function (value) {
-        	/* Checks for '=' sign on each line in order to ignore empty lines and "#EnvironmentSource" */
+            /* Checks for '=' sign on each line in order to ignore empty lines and "#EnvironmentSource" */
             if (value.indexOf('=') !== -1) {
-            	var terms = value.split('=');
+                var terms = value.split('=');
                 result[terms[0]] = terms[1];
             }
         });
@@ -119,15 +121,15 @@ function API(agent, appmetrics) {
         /*
          * MemorySource,1415976582652,totalphysicalmemory=16725618688,physicalmemory=52428800,privatememory=374747136,virtualmemory=374747136,freephysicalmemory=1591525376
          */
-        var values = message.split(/[,=]+/);
-        var physicalTotal = parseInt(values[3]);
-        var physicalFree = parseInt(values[11]);
-        var physicalUsed = (physicalTotal >= 0 && physicalFree >= 0) ? (physicalTotal - physicalFree) : -1;
-        var memory = {time: parseInt(values[1]), physical_total: physicalTotal, physical_used: physicalUsed, physical: parseInt(values[5]), private: parseInt(values[7]), virtual: parseInt(values[9]), physical_free: physicalFree};
-        that.emit('memory', memory);
-    };
+         var values = message.split(/[,=]+/);
+         var physicalTotal = parseInt(values[3]);
+         var physicalFree = parseInt(values[11]);
+         var physicalUsed = (physicalTotal >= 0 && physicalFree >= 0) ? (physicalTotal - physicalFree) : -1;
+         var memory = {time: parseInt(values[1]), physical_total: physicalTotal, physical_used: physicalUsed, physical: parseInt(values[5]), private: parseInt(values[7]), virtual: parseInt(values[9]), physical_free: physicalFree};
+         that.emit('memory', memory);
+     };
 
-    var formatGC = function(message) {
+     var formatGC = function(message) {
         /* gc_node : NodeGCData,1413903289280,S,48948480,13828320,7
          *                     , timestamp   ,M|S, size , used   , pause (ms)
          *
@@ -135,75 +137,93 @@ function API(agent, appmetrics) {
          * so first separate the lines, followed by the normal parsing.
          *
          */
-        var lines = message.trim().split('\n');
-		/* Split each line into the comma-separated values. */
-	    lines.forEach(function (line) {
-        	var values = line.split(/[,]+/);
+         var lines = message.trim().split('\n');
+         /* Split each line into the comma-separated values. */
+         lines.forEach(function (line) {
+            var values = line.split(/[,]+/);
             var gc = {time: parseInt(values[1]), type: values[2], size: parseInt(values[3]), used: parseInt(values[4]), duration: parseInt(values[5])};
             that.emit('gc', gc);
         });
+     };
+
+     var formatHeap = function(message) {
+        /* heap_node: NodeHeapData,9472608,4934136
+        *                         ,total heap size, used heap size
+        *
+        * Heap data can come in batches of multiple lines like the one in the example,
+        * so first separate the lines, followed by the normal parsing.
+        *
+        */
+        var lines = message.trim().split('\n');
+        /* Split each line into the comma-separated values. */
+        lines.forEach(function (line) {
+            var values = line.split(/[,]+/);
+            var heap = {total: parseInt(values[1]), used: parseInt(values[2])};
+            that.emit('heap', heap);
+        });
+
+    }
+
+    var formatProfiling = function (message) {
+        var lines = message.trim().split('\n');
+        var prof = {
+            date: 0,
+            functions: [],
+        };
+        lines.forEach(function (line) {
+            var values = line.split(',');
+            if (values[1] == 'Node') {
+                prof.functions.push({self: parseInt(values[2]), parent: parseInt(values[3]), file: values[4], name: values[5], line: parseInt(values[6]), count: parseInt(values[7])});
+            } else if (values[1] == 'Start') {
+                prof.time = parseInt(values[2]);
+            }
+        });
+        that.emit('profiling', prof);   
     };
 
-	var formatProfiling = function (message) {
-		var lines = message.trim().split('\n');
-		var prof = {
-			date: 0,
-			functions: [],
-		};
-		lines.forEach(function (line) {
-			var values = line.split(',');
-			if (values[1] == 'Node') {
-				prof.functions.push({self: parseInt(values[2]), parent: parseInt(values[3]), file: values[4], name: values[5], line: parseInt(values[6]), count: parseInt(values[7])});
-			} else if (values[1] == 'Start') {
-				prof.time = parseInt(values[2]);
-			}
-		});
-	 	that.emit('profiling', prof);	
-	};
-
     var formatApi = function (message) {
-    	var lines = message.trim().split('\n');
-    	lines.forEach(function (line) {
-    	    var parts = line.split(/:(.+)/);
+        var lines = message.trim().split('\n');
+        lines.forEach(function (line) {
+            var parts = line.split(/:(.+)/);
             var topic = parts[0];
             var data = serializer.deserialize(parts[1]);
-            that.emit(topic, data);	
-    	});
+            that.emit(topic, data); 
+        });
     };
 
     agent.localConnect(function events(topic, data) {
-    	if (topic === 'api') {
-    		// API events are passed by copy
-    		return;
-    	}
-       	message = data.toString();
-       	raiseEvent(topic, message);
+        if (topic === 'api') {
+            // API events are passed by copy
+            return;
+        }
+        message = data.toString();
+        raiseEvent(topic, message);
     });
 //    agent.sendControlCommand("history", "");
 }
 module.exports.getAPI = function(agent, appmetrics) {
-	return new API(agent, appmetrics);
+    return new API(agent, appmetrics);
 };
 
 util.inherits(API, EventEmitter);
 
 API.prototype.enable = function (data) {
-	var that = this;
+    var that = this;
     that.appmetrics.enable(data); 
 };
 
 API.prototype.disable = function (data) {
-	var that = this;
+    var that = this;
     that.appmetrics.disable(data); 
 };
 
 API.prototype.getEnvironment = function() {
-	var that = this;
-	return that.environment;
+    var that = this;
+    return that.environment;
 };
 
 API.prototype.raiseLocalEvent = function(topic, data) {
-	var self = this;
-	self.emit(topic, data);
+    var self = this;
+    self.emit(topic, data);
 };
 
