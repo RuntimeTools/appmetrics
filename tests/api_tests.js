7c7ef8e93557008c3454080cf4ce33c638b46325
aaroncollins <acollins@uk.ibm.com>
2016-05-20 11:47:05 +0100

    Fixed bug in testValuesGreaterThan

--- tests/api_tests.js
+++ tests/api_tests.js
@@ -366,7 +366,7 @@ function runProfilingTests(profData, t){
 	
 	function testValuesAreGreaterThan(keyName, val){
 		for (var index in functions){
-			if (!functions[index][keyName] > val) {
+			if (!(functions[index][keyName] > val)) {
 				t.fail("Value of '" + keyName + "' should be greater than "+ val +" ("+functions[index][keyName]+")");
 				return;
 			}
