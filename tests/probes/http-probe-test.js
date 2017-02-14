e7a2f200e71dcba91921d27eef25a6cf0069c05c
T. Corbin <corbint@uk.ibm.com>
2017-02-14 16:22:38 +0000

    add additional HTTP tests

--- tests/probes/http-probe-test.js
+++ tests/probes/http-probe-test.js
@@ -44,20 +44,17 @@ function checkHttpData(data, t) {
     t.ok(isInteger(data.time), "Timestamp is an integer");
     t.equals(data.method, "GET", "Should report GET as HTTP request method");
     t.equals(data.url, "/", "Should report / as URL");
-    t.equals(
-            data.hasOwnProperty('duration'), true,
+    t.equals(data.hasOwnProperty('duration'), true,
             "Should have HTTP property duration;")
-    t.equals(
-            data.hasOwnProperty('header'), true,
+    t.ok(isNumeric(data.duration), "duration is a number");
+    t.equals(data.hasOwnProperty('header'), true,
             "Should have HTTP property header;")
-    t.equals(
-            data.hasOwnProperty('statusCode'), true,
+    t.equals(data.hasOwnProperty('statusCode'), true,
             "Should have HTTP property statusCode;")
-    t.equals(
-            data.hasOwnProperty('contentType'), true,
+    t.ok(isInteger(data.statusCode), "statusCode is an integer");
+    t.equals(data.hasOwnProperty('contentType'), true,
             "Should have HTTP property contentType;")
-    t.equals(
-            data.hasOwnProperty('requestHeader'), true,
+    t.equals(data.hasOwnProperty('requestHeader'), true,
             "Should have HTTP property requestHeader;")
 
 }
