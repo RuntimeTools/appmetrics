aff1e7850ad680a0969d941da6a7cff30bde1761
MatthewColegate <mcolegate@aol.com>
2017-04-11 16:18:39 +0100

    Bump version number to 3.0.2 for development

--- extract_all_binaries.js
+++ extract_all_binaries.js
@@ -39,7 +39,7 @@ var AGENTCORE_PLATFORMS = ['aix-ppc',
                            'win32-ia32',
                            'win32-x64'];
 var AGENTCORE_VERSION = "3.2.1";
-var APPMETRICS_VERSION = "3.0.1";
+var APPMETRICS_VERSION = "3.0.2";
 
 var LOG_FILE = path.join(INSTALL_DIR, 'install.log');
 var logFileStream = fs.createWriteStream(LOG_FILE, {flags : 'a'});
