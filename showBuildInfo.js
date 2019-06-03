e764d6283f0b56a08aed1a0d1b921bbbae439442
Richard Waller <richard.lh.waller@gmail.com>
2019-06-03 11:35:32 +0100

    Update showBuildInfo.js
    Co-Authored-By: Matt Colegate <colegate@uk.ibm.com>

--- showBuildInfo.js
+++ showBuildInfo.js
@@ -37,7 +37,7 @@ function showBuildWarning() {
   console.log('\n');
   console.log('********************************************************************************');
   // eslint-disable-next-line max-len
-  console.log('You are about to build a load of binaries. If this build errors, make sure you have the node-gyp pre-requisites \(https://github.com/nodejs/node-gyp#installation). If you have them and the build still errors, see if there are any related issues at https://github.com/RuntimeTools/appmetrics/issues). If there aren\'t, feel free to open a new issue to report the bug.');
+  console.log('Appmetrics uses node-gyp to compile and build local binary libraries to enhance execution performance. If the following compilation and build logs contain errors, make sure you have the node-gyp pre-requisites installed \(https://github.com/nodejs/node-gyp#installation). If you have them and the build still had errors, see if there are any related issues at https://github.com/RuntimeTools/appmetrics/issues). If there aren\'t, feel free to open a new issue to report the bug.');
   console.log('********************************************************************************');
   console.log('\n');
 };
