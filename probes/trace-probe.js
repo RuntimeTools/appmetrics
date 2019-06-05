8ee57e33c2aca1bedaf117c9c3fa0f68548574a0
YUE CHEN <yuecchen@cn.ibm.com>
2019-06-05 13:21:35 +0800

    fix #585

--- probes/trace-probe.js
+++ probes/trace-probe.js
@@ -263,7 +263,7 @@ function isAppInnerRequire() {
   var trace = {};
   Error.captureStackTrace(trace);
   var callerLine = trace.stack.split('\n'); // This line contains 'node_modules' reference for generic libs
-  return callerLine[6].indexOf('node_modules') == -1;
+  return callerLine[6].indexOf('node_modules') == -1 && callerLine[6].indexOf('ibmapm') == -1;
 }
 
 function instrumentMethods(moduleName, target) {
